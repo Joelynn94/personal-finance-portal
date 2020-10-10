4a5e427f549043f723dee605ea6206e6daf0b96f
Joseph Lynn <josephlynn94@gmail.com>
2020-10-10 14:06:06 -0400

    revert back to basic server setup

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 
-// Requiring our models for syncing
-const db = require('./app/models')
-
 const corsOptions = {
   origin: 'http://localhost:8080',
 }
@@ -24,12 +21,6 @@ app.get('/', (req, res) => {
   res.json({ message: 'basic server is setup' })
 })
 
-// require the routes
-require('./app/routes/debt.routes')(app)
-
-db.sequelize.sync({ force: true }).then(() => {
-  console.log('Drop and re-sync db.')
-})
 // listen for requests
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)

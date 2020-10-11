63d0de48618bce5f7ccb6f456a7c4d30707e7839
Joseph Lynn <josephlynn94@gmail.com>
2020-10-11 12:35:55 -0400

    made simple API CRUD routes to make sure they are working, uninstalled sequelize

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const dotenv = require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
@@ -10,15 +11,59 @@ const PORT = process.env.PORT || 8080
 
 // setup the middleware to handle data parsing
 app.use(express.urlencoded({ extended: true }))
+// this middleware creates the req.body object
 app.use(express.json())
 // use CORS
 app.use(cors(corsOptions))
 // use static files
 app.use(express.static('public'))
 
-// basic route
-app.get('/', (req, res) => {
-  res.json({ message: 'basic server is setup' })
+// get all debts
+app.get('/api/v1/debts', (req, res) => {
+  res.status(200).json({
+    status: "success",
+    debt: ['Student Loan', 'Auto Loan']
+  })
+})
+
+// get one debt by id
+app.get('/api/v1/debts/:id', (req, res) => {
+  console.log(req.params.id)
+
+  res.status(200).json({
+    status: "success",
+    debt: ['Student Loan', 'Auto Loan']
+  })
+})
+
+// post a new debt
+app.post('/api/v1/debts', (req, res) => {
+  console.log(req.body)
+
+  res.status(201).json({
+    status: "success",
+    debt: ['Student Loan', 'Auto Loan']
+  })
+})
+
+// update a deby by id
+app.put('/api/v1/debts/:id', (req, res) => {
+  console.log(req.params.id)
+  console.log(req.body)
+
+  res.status(200).json({
+    status: "success",
+    debt: ['Student Loan', 'Auto Loan']
+  })
+})
+
+app.delete('/api/v1/debts/:id', (req, res) => {
+  console.log(req.params.id)
+  console.log(req.body)
+
+  res.status(204).json({
+    status: "success"
+  })
 })
 
 // listen for requests

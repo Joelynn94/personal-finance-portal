b93217073758d9052c225af71bdc62a995443414
Joseph Lynn <josephlynn94@gmail.com>
2021-01-09 09:45:00 -0500

    fixed the get route to get a user with a token

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -1,27 +1,31 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res, next) => {
-  // checking the request header for a token
-  // const { token } = req.headers('x-auth-token');
   let token = '';
 
   // if there is not token
   if (
     req.headers.authorization &&
-    req.headers.authorization.split(' ')[0] === 'Bearer'
+    req.headers.authorization.startsWith('Bearer')
   ) {
     try {
       token = req.headers.authorization.split(' ')[1];
       // checks if the jwt token is valid
-      const payload = jwt.verify(token, process.env.jwtSecret);
+      const user = jwt.verify(token, process.env.jwtSecret);
 
       // puts this payload into the user object (req.user) - so we can access this in our routes
-      req.user = payload.user;
+      req.user = user;
       next();
     } catch (error) {
       res.status(403).json({
         message: 'Not Authorized',
       });
     }
+  }
+
+  if (!token) {
+    res.status(401).json({
+      status: 'Not authorized, no token',
+    });
   }
 };
--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -2,22 +2,9 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
   // create a json web token
-  return jwt.sign(
-    { id },
-    process.env.jwtSecret,
-    {
-      expiresIn: 5000,
-    },
-    (err, token) => {
-      if (err) {
-        return res
-          .status(401)
-          .send({ status: `There was a problem generating a web token` });
-      }
-      // if the data was created - send the new data back and the token
-      res.status(201).json({ id, token });
-    }
-  );
+  return jwt.sign({ id }, process.env.jwtSecret, {
+    expiresIn: '30d',
+  });
 };
 
 module.exports = generateToken;
